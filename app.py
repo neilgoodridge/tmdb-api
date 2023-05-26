3ff5792ae7fb6a8a983e142eae9a4a65b0e5b7ba
neilgoodridge <neilrgoodridge@gmail.com>
2023-05-26 23:42:40 +0100

    logic for top films

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 from api_key import API_KEY
+import requests
 
-api_key = {API_KEY}
-# print(api_key)
-
+base_url = 'https://api.themoviedb.org/3'
 app = Flask(__name__)
 
+def get_top_films(api_key):
+    endpoint = '/movie/top_rated'
+
+    url = f'{base_url}{endpoint}?api_key={api_key}'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        top_films = data['results']
+        return top_films
+    else:
+        print('Error')
+
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def top_films():
+    films = get_top_films(API_KEY)
+    return render_template('index.html', films=films)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
